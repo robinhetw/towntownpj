2589f3dc2bb91fb244cdac3b3af1a43d330549b6
Robin He <31844669+robinhetw@users.noreply.github.com>
2018-09-17 00:03:45 +0800

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+
+/*幻燈片*/
+$("#example, .main").vegas({
+    timer: false,
+    slides: [
+        { src: "css/img/slider.png" },
+        { src: "css/img/slider2.png" }
+    ],
+    animation: 'kenburnsUpRight'
+});
+
+/*視差捲動*/
+
+//var scene = document.getElementById('scene');
+//var parallaxInstance = new Parallax(scene);
