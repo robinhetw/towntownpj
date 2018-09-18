4ab91186685d5db3ac2ffe615ca474c8951c5a7b
Robin He <31844669+robinhetw@users.noreply.github.com>
2018-09-18 22:56:56 +0800

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,15 +1,35 @@
-
 /*幻燈片*/
 $("#example, .main").vegas({
-    timer: false,
-    slides: [
-        { src: "css/img/slider.png" },
-        { src: "css/img/slider2.png" }
-    ],
-    animation: 'kenburnsUpRight'
+  timer: false,
+  slides: [{ src: "css/img/slider.png" }, { src: "css/img/slider2.png" }],
+  animation: "kenburnsUpRight"
 });
 
 /*視差捲動*/
 
 //var scene = document.getElementById('scene');
 //var parallaxInstance = new Parallax(scene);
+
+/*light box*/
+$(document).ready(function () {
+  $("#lightSlider").lightSlider({
+      auto: true,
+      loop: true,
+      pauseOnHover: true
+  });
+});
+
+$('#imageGallery').lightSlider({
+  gallery:true,
+  item:1,
+  loop:true,
+  thumbItem:9,
+  slideMargin:0,
+  enableDrag: false,
+  currentPagerPosition:'left',
+  onSliderLoad: function(el) {
+      el.lightGallery({
+          selector: '#imageGallery .lslide'
+      });
+  }   
+});  
