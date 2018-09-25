b0d4a5d1d3d6689cbf6f7daf39ccbea49b346682
Robin He <31844669+robinhetw@users.noreply.github.com>
2018-09-25 11:13:32 +0800

    Add files via upload

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,12 @@
 /*幻燈片*/
 $("#example, .main").vegas({
   timer: false,
-  slides: [{ src: "css/img/1600_930_slider1.jpg" },{ src: "css/img/1600_930_slider2.jpg" },
-  { src: "css/img/1600_930_slider3.jpg" }],
- 
+  slides: [
+    { src: "css/img/1600_930_slider1.jpg" },
+    { src: "css/img/1600_930_slider2.jpg" },
+    { src: "css/img/1600_930_slider3.jpg" }
+  ],
+
   animation: "random",
   overlay: "JS/vegas/overlays/06.png"
 });
@@ -16,7 +19,7 @@ $("#example, .main").vegas({
 /*light box*/
 $(document).ready(function() {
   $("#lightSlider").lightSlider({
-    autoWidth:true,
+    autoWidth: true,
     auto: true,
     loop: true,
     pauseOnHover: true
@@ -52,30 +55,80 @@ $(function() {
 /* 變換 logo size */
 
 $(window).scroll(function() {
-  if ($(document).scrollTop()> 20) {
-    $('#logo').addClass('logoChangeImg');
-    $('#logo').removeClass('logoTopImg ');
+  if ($(document).scrollTop() > 20) {
+    $("#logo").addClass("logoChangeImg");
+    $("#logo").removeClass("logoTopImg ");
   } else {
-    $('#logo').removeClass('logoChangeImg');
-    $('#logo').addClass('logoTopImg');
+    $("#logo").removeClass("logoChangeImg");
+    $("#logo").addClass("logoTopImg");
   }
 });
 
 /* read more*/
-$('#readMoreButton').click(function() {
+$("#readMoreButton").click(function() {
   /*$('#readMoreBlock').removeClass('readClose');
   $('#readMoreBlock').addClass('readOpen');*/
-  $('#readMoreBlock').show(300);
-  $('#readMoreButton').remove();
-
+  $("#readMoreBlock").show(300);
+  $("#readMoreButton").remove();
 });
 /* menu*/
 
-(function () {
-	$('.menu-wrapper').on('click', function() {
-    $('.hamburger-menu').toggleClass('animate');
+(function() {
+  $(".menu-wrapper").on("click", function() {
+    $(".hamburger-menu").toggleClass("animate");
     $("#js-open").toggleClass("active");
-	})
+  });
 })();
 
+/*animated move */
 
+$(document).ready(function() {
+  $(".js--wp--1").waypoint(function() {
+      $(".js--wp--1").addClass("animated fadeInUp");
+    },
+    {
+      offset: "60%"
+    }
+  );
+
+  $(".js--wp--2").waypoint(function() {
+    $(".js--wp--2").addClass("animated fadeInUp");
+  },
+  {
+    offset: "60%"
+  }
+);
+
+$(".js--wp--3").waypoint(function() {
+  $(".js--wp--3").addClass("animated fadeInUp");
+},
+{
+  offset: "60%"
+}
+);
+$(".js--wp--4").waypoint(function() {
+  $(".js--wp--4").addClass("animated fadeInUp");
+},
+{
+  offset: "60%"
+}
+);
+
+$(".js--wp--5").waypoint(function() {
+  $(".js--wp--5").addClass("animated fadeInLeft");
+},
+{
+  offset: "60%"
+}
+);
+
+$(".js--wp--6").waypoint(function() {
+  $(".js--wp--6").addClass("animated fadeInRight");
+},
+{
+  offset: "60%"
+}
+);
+
+
+});
